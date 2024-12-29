68fdcd91473693291972a1ae06ad2d9ac444170f
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-29 14:50:52 +0700

    Menambahkan interface MahasiswaService

---
+++ app/src/main/java/com/example/pam_remotedb_andini/service_api/MahasiswaService.kt
@@ -0,0 +1,20 @@
+package com.example.pam_remotedb_andini.service_api
+
+import com.example.pam_remotedb_andini.model.Mahasiswa
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface MahasiswaService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+
+
+}
