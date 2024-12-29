7fd939d5402f024164c68245837a5e4b9cfc945b
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-29 15:17:31 +0700

    Menambahkan fungsi OnError

--- app/src/main/java/com/example/pam_remotedb_andini/ui/view/HomeView.kt
+++ app/src/main/java/com/example/pam_remotedb_andini/ui/view/HomeView.kt
@@ -52,6 +52,23 @@ object DestinasiHome : DestinasiNavigasi {
 
 
 @Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column (
+        modifier =  modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
 fun MhsLayout(
     mahasiswa: List<Mahasiswa>,
     modifier: Modifier = Modifier,
