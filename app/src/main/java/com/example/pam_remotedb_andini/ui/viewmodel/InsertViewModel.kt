18a5b22d2e7fa1031c2eb554c35765cb9cfa9e5d
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-29 15:04:23 +0700

    Menambahkan kelas InsertViewModel

--- app/src/main/java/com/example/pam_remotedb_andini/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pam_remotedb_andini/ui/viewmodel/InsertViewModel.kt
@@ -9,7 +9,24 @@ import com.example.pam_remotedb_andini.model.Mahasiswa
 import com.example.pam_remotedb_andini.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 
+class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+        private set
 
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent) {
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertMhs() {
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            } catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
     nim = nim,
