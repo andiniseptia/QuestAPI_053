45dec29c39b3e220b5309885c87b8c75ce261e62
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-29 14:54:24 +0700

    Menambahkan interface MahasiswaRepository

---
+++ app/src/main/java/com/example/pam_remotedb_andini/repository/MahasiswaRepository.kt
@@ -0,0 +1,13 @@
+package com.example.pam_remotedb_andini.repository
+
+import com.example.pam_remotedb_andini.model.Mahasiswa
+import com.example.pam_remotedb_andini.service_api.MahasiswaService
+import okio.IOException
+
+interface MahasiswaRepository {
+    suspend fun getMahasiswa(): List<Mahasiswa>
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
+    suspend fun deleteMahasiswa(nim: String)
+    suspend fun getMahasiswaById(nim: String): Mahasiswa
+}
