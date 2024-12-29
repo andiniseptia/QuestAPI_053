a2ed0dee3cc025940ddfd0016aa59b69e5db7fc7
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-29 15:22:01 +0700

    Menambahkan fungsi PengelolaHalaman dengan NavHost

---
+++ app/src/main/java/com/example/pam_remotedb_andini/navigation/PengelolaHalaman.kt
@@ -0,0 +1,23 @@
+package com.example.pam_remotedb_andini.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pam_remotedb_andini.ui.view.DestinasiEntry
+import com.example.pam_remotedb_andini.ui.view.DestinasiHome
+import com.example.pam_remotedb_andini.ui.view.EntryBody
+import com.example.pam_remotedb_andini.ui.view.EntryMhsScreen
+import com.example.pam_remotedb_andini.ui.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    )
+}
