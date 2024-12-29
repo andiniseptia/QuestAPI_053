517a957bfc6bca907b0f472d957e2a52b9fc7b69
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-29 15:01:59 +0700

    Menambahkan data class InsertUiEvent

---
+++ app/src/main/java/com/example/pam_remotedb_andini/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.pam_remotedb_andini.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pam_remotedb_andini.model.Mahasiswa
+import com.example.pam_remotedb_andini.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
