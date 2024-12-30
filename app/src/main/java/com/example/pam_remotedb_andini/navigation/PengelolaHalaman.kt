f61c45658eb3c143a6d80d91e38601c1d897228d
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-30 09:14:32 +0700

    Menambahkan navigasi ke DetailView dengan parameter NIM

--- app/src/main/java/com/example/pam_remotedb_andini/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pam_remotedb_andini/navigation/PengelolaHalaman.kt
@@ -9,9 +9,11 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.pam_remotedb_andini.ui.view.DestinasiEntry
 import com.example.pam_remotedb_andini.ui.view.DestinasiHome
+import com.example.pam_remotedb_andini.ui.view.DetailView
 import com.example.pam_remotedb_andini.ui.view.EntryBody
 import com.example.pam_remotedb_andini.ui.view.EntryMhsScreen
 import com.example.pam_remotedb_andini.ui.view.HomeScreen
+import com.example.pam_roomlocaldb_andini.ui.view.mahasiswa.UpdateView
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
@@ -23,7 +25,11 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
         composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
-                onDetailClick = {
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+                    println(
+                        "PengelolaHalaman: nim = $nim"
+                    )
                 }
             )
         }
@@ -37,5 +43,19 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                 }
             })
         }
+
+        composable(
+            "${DestinasiDetail.route}/{nim}"
+        ) {
+            navBackStackEntry ->
+            val nim = navBackStackEntry.arguments?.getString("nim")
+            nim?.let {
+                DetailView(nim = it, onBack = { navController.popBackStack() }, onEditClick = { nim ->
+                    navController.navigate("${DestinasiUpdate.route}/$nim")
+                })
+            }
+        }
+
+
     }
 }
