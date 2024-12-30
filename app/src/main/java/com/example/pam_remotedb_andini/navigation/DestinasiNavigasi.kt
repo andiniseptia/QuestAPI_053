95af73589d7340f2cf1e85820802136df4a5471a
andiniseptia <andiniseptiandri124@gmail.com>
2024-12-30 09:13:41 +0700

    Menambah objek DestinasiDetail untuk navigasi

--- app/src/main/java/com/example/pam_remotedb_andini/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/pam_remotedb_andini/navigation/DestinasiNavigasi.kt
@@ -4,3 +4,8 @@ interface DestinasiNavigasi {
     val route: String
     val titleRes: String
 }
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = "Detail Mahasiswa"
+}
